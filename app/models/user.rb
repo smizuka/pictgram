470b9e7eb150ebab9c8d7c8309c95e9dc630a0a4
smizuka <satomizu2@hotmail.com>
2019-01-21 19:17:36 +0900

    task190121

---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ApplicationRecord
+
+  # 各フォームに値が入っているか確認
+  validates :name,  presence: { message: "%{value} を入力してください" }
+  validates :email, presence: { message: "%{value} を入力してください" }
+  validates :password, presence: { message: "%{value} を入力してください" }
+
+  #名前の文字数制限
+  validates :name, length: { maximum: 15,message: "は15字以内にしてください" }
+
+  #メールの形式に関する制限
+  validates :email, format: { with: /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/,
+    message: "は~ @ ~ . ~の表現にしてください" }
+
+  #パスワードの文字数制限
+  validates :password, length: { in: 8..32, too_long:"は３２文字以内にしてください", too_short:"は８文字以上にしてください"}
+
+  validates :password, format: { with: /(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)/,
+    message: "%{value}はアルファべット（大小）と半角数字の混合にしてください" }
+
+  #パスワードが一致しているかの確認
+  # validates :password, confirmation: true
+  validates :password, confirmation: { message: "とpasswordの値が一致していません" }
+  # validates :password_confirmation, presence: true
+
+  has_secure_password
+
+end
+
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  get 'sessions/new'
+  # get 'users/new'
+
+  root "pages#index"
+  get 'pages/help'
+
+  resources :users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+end
